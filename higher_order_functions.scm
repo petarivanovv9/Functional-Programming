1e1d50a8b7e725b31eabd79b5b044db36c62b555
Petar Ivanov <pepincho95@gmail.com>
2016-10-30 16:49:18 +0200

    Add higher order functions.

---
+++ higher_order_functions.scm
@@ -0,0 +1,84 @@
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(define (sum-e a b)
+  (if (> a b)
+      0
+      (+ (/ 1 (fact a)) (sum-e (+ a 1) b))))
+
+; Напишете функция от по-висок ред (sum term a next b), 
+; която улавя общото от трите дефиниции и параметризира специфичното
+
+(define (identity x) x)
+(define (1+ x) (+ 1 x))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a) (sum term (next a) next b))
+  )
+)
+
+; > (sum identity 1 1+ 10)
+; 55
+
+(define (sum-integers a b)
+  (sum identity a add1 b))
+
+(define (sum-cubes a b)
+  (sum (lambda (x) (* x x x)) a add1 b))
+
+(define (sum-e a b)
+  (sum (lambda (x) (/ 1 (fact x))) a 1+ b))
+
+; problem 3
+
+;(define (integral f a b delta)  
+;
+;)
+
+; problem 4
+
+(define (sum2 term a next b)
+  (define (iter current result)
+    (if (> current b)
+  	result
+  	(iter (next current) (term current result))
+    )
+  )
+  (iter a 0)
+)
+
+; > (sum2 + 1 1+ 10)
+; 55
+
+; problem 7
+
+(define (accumulate op nv a b term next)
+  (if (> a b)
+      nv
+      (op (term a) (accumulate op nv (next a) b term next))
+  )
+)
+
+(define (filtered-accumulate op nv pred term a next b)
+  (cond 
+    ((> a b) nv)
+    ((not (pred a)) (filtered-accumulate op nv pred term (next a) next b))
+    (else (op (term a) (filtered-accumulate op nv pred term (next a) next b)))
+  )
+)
+
+(define (odd-2? x)
+  (not (= (remainder x 2) 0))
+)
+
+; > (filtered-accumulate + 0 odd-2? identity 1 1+ 10)
+; 25
