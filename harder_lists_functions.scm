1f31baa8ec4280d910f5eea48419d7065ee14f4f
Petar Ivanov <pepincho95@gmail.com>
2016-11-06 15:15:22 +0200

    Update harder-list-functions with filter list func.

--- harder_lists_functions.scm
+++ harder_lists_functions.scm
@@ -10,4 +10,19 @@
 ; > (my-map (lambda (x) (* x x)) '(1 2 3 4))
 ; (1 4 9 16)
 
+;;; (filter pred lst)
 
+(define (my-filter pred? l)
+  (cond
+    ((null? l) '())
+    ((not (pred? (car l))) (my-filter pred? (cdr l)))
+    (else (cons (car l) (my-filter pred? (cdr l))))
+  )
+)
+
+; > (my-filter odd? '(1 2 3 4 5 6 7 8 9))
+; (1 3 5 7 9)
+; > (my-filter even? '(1 2 3 4 5 6 7 8 9))
+; (2 4 6 8)
+; > (my-filter (lambda (x) x) '(1 2 3 4 5 6 7 8 9))
+; (1 2 3 4 5 6 7 8 9)
