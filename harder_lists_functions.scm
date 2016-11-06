87db4919b2623218e4487416de06db22da9c8e41
Petar Ivanov <pepincho95@gmail.com>
2016-11-06 15:43:26 +0200

    Update harder-list-functions with selection-sort on list func.

--- harder_lists_functions.scm
+++ harder_lists_functions.scm
@@ -39,5 +39,45 @@
 
 ; > (my-remove (list 1 2 1 3 1 4) 1)
 ; (2 3 4)
-; (my-remove (list 1 2 1 3 1 4) 5)
+; > (my-remove (list 1 2 1 3 1 4) 5)
 ; (1 2 1 3 1 4)
+
+;;; selection sort
+
+(define (min-elem l)
+  (cond
+    ((null? (cdr l)) (car l))
+    ((< (car l) (min-elem (cdr l))) (car l))
+    (else (min-elem (cdr l)))
+  )
+)
+
+; > (min-elem (list 1 2 3 4 5 0))
+; 0
+
+(define (remove-first-min-elem l elem)
+  (if (null? l)
+      '()
+      (if (= (car l) elem)
+          (cdr l)
+          (cons (car l) (remove-first-min-elem (cdr l) elem))
+      )
+  )
+)
+
+; > (remove-first-min-elem (list 1 2 3 1 4 5) (min-elem (list 1 2 3 1 4 5)))
+; (2 3 1 4 5)
+
+(define (sel-sort l)
+  (if (null? l)
+      '()
+      (cons (min-elem l) (sel-sort (remove-first-min-elem l (min-elem l))))
+  )
+)
+
+; > (sel-sort '(7 6 5 4 3 2 1))
+; (1 2 3 4 5 6 7)
+; > (sel-sort (list 1 2 3 8 6 5 0 8 9 6 7 6 5))
+; (0 1 2 3 5 5 6 6 6 7 8 8 9)
+; > (sel-sort (list 1 2 3 4))
+; (1 2 3 4)
