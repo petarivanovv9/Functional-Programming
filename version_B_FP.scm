89963229a82a5633c3dbe6eb1119752cc9a2e6fd
Petar Ivanov <pepincho95@gmail.com>
2016-11-14 10:52:53 +0200

    Add task 2 from version_B_FP.

--- version_B_FP.scm
+++ version_B_FP.scm
@@ -34,3 +34,66 @@
   (and (>= (mixed?-helper-1 f g a b) 1) (>= (mixed?-helper-2 f g a b) 1))
 )
 
+;;; task 2
+
+(define (member? x l)
+  (cond
+    ((null? l) #f)
+    ((= (car l) x) #t)
+    (else (member? x (cdr l)))
+  )
+)
+
+(define (filter pred? l)
+  (cond
+    ((null? l) '())
+    ((pred? (car l)) (cons (car l) (filter pred? (cdr l))))
+    (else (filter pred? (cdr l)))
+  )
+)
+
+(define (filter-more-than-one l)
+  (define (helper res l)
+    (cond
+      ((null? l) res)
+      ((and (member (car l) (cdr l)) (not (member (car l) res))) (helper (cons (car l) res) (cdr l)))
+      (else (helper res (cdr l)))
+    )
+  )
+  (helper '() l)
+)
+
+(define (filter-unique lst-all)
+  (define lst-more-than-one (filter-more-than-one lst-all))
+  (define (helper res lst-all lst-more-than-one)
+    (cond
+      ((null? lst-all) res)
+      ((member (car lst-all) lst-more-than-one) (helper res (cdr lst-all) lst-more-than-one))
+      (else (helper (cons (car lst-all) res) (cdr lst-all) lst-more-than-one))
+    )
+  )
+  (helper '() lst-all lst-more-than-one)
+)
+
+(define (foldr op nv l)
+  (if (null? l)
+      nv
+      (op (car l) (foldr op nv (cdr l)))
+  )
+)
+
+(define (max-of-list l)
+  (if (null? l)
+      #f
+      (foldr max (car l) l)
+  )
+)
+
+(define (maxUnique ll)
+  (max-of-list 
+    (filter 
+       integer? 
+       (map (lambda (l) (max-of-list (filter-unique l))) ll)
+    )
+  )
+)
