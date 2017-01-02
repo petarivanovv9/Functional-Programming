ab9df7b54d0b9b3e227bdf86117d18d6c14cd67b
Petar Ivanov <pepincho95@gmail.com>
2017-01-02 19:39:55 +0200

    Add initial project solution.

--- Project/solution.scm
+++ Project/solution.scm
@@ -1,12 +1,21 @@
-;(define (getRandomSentence))
+(#%require (only racket/base random))
+(#%require (only racket/base string-titlecase))
+
+;(define temp 
+;  (list 
+;    (list "към небето" "д") (list "куче" "п" "ср") (list "лае" "с" "ед") 
+;    (list "птиците" "д") (list "зелените" "о" "мн") (list "жаби" "п" "мн") 
+;    (list "гледат" "с" "мн") (list "голямото" "о" "ср")))
 
 (define temp 
-  (list 
-    (list "към небето" "д") (list "куче" "п" "ср") (list "лае" "с" "ед") 
-    (list "птиците" "д") (list "зелените" "о" "мн") (list "жаби" "п" "мн") 
-    (list "гледат" "с" "мн") (list "голямото" "о" "ср")
-  )
-)
+  '( 
+    ("към небето" "д") ("куче" "п" "ср") ("лае" "с" "ед") 
+    ("птиците" "д") ("зелените" "о" "мн") ("жаби" "п" "мн") 
+    ("гледат" "с" "мн") ("голямото" "о" "ср")))
+
+;(define temp 
+;  (list 
+;    (list "към небето" "д") (list "куче" "п" "ср") (list "лае" "с" "ед") (list "зелените" "о" "мн")))
 
 (define (my-filter pred? l)
   (cond
@@ -43,3 +52,54 @@
 (define (extractAdverbs l)
   (my-filter (lambda (x) (if (string=? (cadr x) "д") #t #f)) l))
 
+(define on-of (lambda (l) (random-elem l)))
+
+(define random-elem
+  (lambda (l) (nth-elem (random (length l)) l)))
+
+(define nth-elem
+  (lambda (n l)
+    (cond ( (= n 0) (car l))
+          ( else (nth-elem (- n 1) (cdr l))))))
+
+(define nouns (extractNouns temp))
+(define adjectives (extractAdjectives temp))
+(define adverbs (extractAdverbs temp))
+(define verbs (extractVerbs temp))
+
+(define getNoun (lambda () (on-of nouns)))
+(define getAdjective (lambda () (on-of adjectives)))
+(define getAdverb (lambda () (on-of adverbs)))
+(define getVerb (lambda () (on-of verbs)))
+
+(define (check-adj-noun adj noun)
+  (if (string=? (caddr adj) (caddr noun))
+      #t #f))
+
+;idea
+;(define singular (....))
+;(member singular ... and (caddr verb))
+
+(define (check-noun-verb noun verb)
+  (if 
+   (or (and (or (or (string=? (caddr noun) "м") (string=? (caddr noun) "ж")) (string=? (caddr noun) "ср")) (string=? (caddr verb) "ед"))
+       (and (string=? (caddr noun) "мн") (string=? (caddr verb) "мн"))
+   )
+      #t
+      #f
+  )
+)
+
+(define getRandomSentence
+  (lambda ()
+    (define noun (getNoun))
+    (define adjective (getAdjective))
+    (define verb (getVerb))
+    (define adverb (getAdverb))
+
+    (if (and (check-adj-noun adjective noun) (check-noun-verb noun verb))
+        ;(apply string-append "" (append (list (string-titlecase (car adjective)) " " (car noun)) (list " " (car verb) " " (car adverb) ".")))
+        (apply string-append "" (append (append (list (string-titlecase (car (list (car adjective) " " (car noun))))) (cdr (list (car adjective) " " (car noun)))) (list " " (car verb) " " (car adverb) ".")))
+        (getRandomSentence))
+  )
+)
