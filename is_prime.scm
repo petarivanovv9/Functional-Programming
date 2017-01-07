5ec23d34bc34271e6da2bd42bb4124886e714273
Petar Ivanov <pepincho95@gmail.com>
2017-01-07 21:09:25 +0200

    Fix typos.

--- is_prime.scm
+++ is_prime.scm
@@ -1,24 +1,24 @@
-(define (has-devisor? n k) 
+(define (has-divisor? n k)
   (cond ((< k 2) #false)
         ((= (modulo n k) 0) #true)
-        (else (has-devisor? n (- k 1)))
+        (else (has-divisor? n (- k 1)))
   )
 )
 
-(define (has-devisor-2? n k)
+(define (has-divisor-2? n k)
   (define (div? n d)
        (and (> d 0) (= (remainder n d) 0))
   )
   (cond ((< k 2) #false)
         ((= (modulo n k) 0) #true)
-        (else (has-devisor? n (- k 1)))
+        (else (has-divisor? n (- k 1)))
   )
 )
- 
+
 (define (prime? n)
-  (not (has-devisor? n (ceiling (sqrt n))))
+  (not (has-divisor? n (ceiling (sqrt n))))
 )
-  
+
 (define (prime-2? n)
-  (not (has-devisor-2? n (ceiling (sqrt n))))
+  (not (has-divisor-2? n (ceiling (sqrt n))))
 )
