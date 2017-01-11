e03a9758091607e699fadfe3e789e6547382eaeb
Petar Ivanov <pepincho95@gmail.com>
2017-01-11 21:06:03 +0200

    Add task-6 on haskell from Homework-1.

---
+++ Homework-1/solution.hs
@@ -0,0 +1,52 @@
+--Task 6
+
+--1) ["strings", "safe", "get", "setting", "elon"]
+
+--1)' [["strings"], ["safe"], ["get"], ["setting"], ["elon"]]
+
+--2) [
+--  ["safe", "strings"], -> наобратно добавяме думите които могат да се конкатенират
+                        -- и винаги ще взимаме head-а
+--  ["strings", "setting"],
+--  ["safe", "elon"],
+--  ....
+--]
+
+--3) [
+--  ["strings", "safe", "elon"],
+--  ["strings", "setting", "get"],
+--]
+
+--4) [ .. ]
+
+lst = ["strings", "safe", "get", "setting", "elon"]
+
+res = makeStrList lst
+
+canConcat s1 s2 = last s1 == head s2
+
+getMaxListWords words = foldr ( \ x res -> if getLengthList x > getLengthList res then x else res ) [] words
+
+--връща ни дължината на списък от думи
+--foldr приема двуместна ф-ия като параметър
+getLengthList lst = foldr ( \ x res -> length x + res ) 0 lst - length lst + 1
+
+--1)'
+makeStrList lst = [ [x] | x<-lst ]
+
+--2)'
+-- взимаме елемент от lst и се опитваме да го конкатенираме към res
+makeWord lst res = [ x : y | x<-lst, y<-res, x /= head y, canConcat x  (head y) ]
+
+genListStrings lst res currMaxList
+  | null res = currMaxList
+  | getLengthList currMaxList < getLengthList currMax = genListStrings lst newRes currMax
+  | otherwise = genListStrings lst newRes currMaxList
+  where newRes = makeWord lst res
+        currMax = getMaxListWords newRes
+
+
+longestWord lst
+  | null wordsList = []
+  | otherwise = foldr1 ( \ x (_ : res) -> x ++ res ) wordsList
+  where wordsList = (genListStrings lst (makeStrList lst) [])
