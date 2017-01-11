89de2abeff43dab1969969d554e32e9e642d588b
Petar Ivanov <pepincho95@gmail.com>
2017-01-11 12:09:31 +0200

    Add add-vertex, add-edge, make-from-edges graph functions.

--- data-structures/graph.scm
+++ data-structures/graph.scm
@@ -1,6 +1,11 @@
 (define (member? x l)
   (if (member x l) #t #f))
 
+(define (foldr op nv l)
+  (if (null? l)
+      nv
+      (op (car l) (foldr op nv (cdr l)))))
+
 ; малък граф, над който ще тестваме всички функции
 (define G '((a b c d) ; от а има ребра към b,c,d
             (b e f)   ; може да бъде и ориентиран
@@ -28,3 +33,30 @@
 ; (has-edge? 'c 'e G) -> #f
 (define (has-edge? u v g)
   (member? v (successors u g)))
+
+
+; task 2
+
+; за да добавяме ребро първо трябва да добавим върховете му
+(define (add-vertex v g)
+  (if (member? v (vertices g))
+      g
+      (cons (list v) g)))
+
+; добавяне на ребро в граф
+(define (add-edge u v g)
+  (if (has-edge? u v g)
+      g
+      (let ((newg (add-vertex u (add-vertex v g))))
+        (map (lambda (l) (if (equal? (car l) u)
+                             (append l (list v))
+                             l))
+             newg))))
+
+; създаване на граф от цял списък с ребра
+(define (make-from-edges l)
+  (foldr (lambda (e g) (add-edge (car e)
+                                 (cdr e)
+                                 g))
+         '()
+         l))
