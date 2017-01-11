62ad0fd41b5322b0e804d6dba49e2ce30d5abdd2
Petar Ivanov <pepincho95@gmail.com>
2017-01-11 11:54:40 +0200

    Add vertices, successors and has-edge? graph functions.

--- data-structures/graph.scm
+++ data-structures/graph.scm
@@ -1,7 +1,7 @@
 (define (member? x l)
   (if (member x l) #t #f))
 
-; маък граф, над който ще тестваме всички функции
+; малък граф, над който ще тестваме всички функции
 (define G '((a b c d) ; от а има ребра към b,c,d
             (b e f)   ; може да бъде и ориентиран
             (c a d)
@@ -9,3 +9,22 @@
             (e)       ; връх без наследници
             (f b e)
             (g a)))
+
+
+; task 1
+
+; взимане на всички върхове в граф
+; (vertices G) -> '(a b c d e f g)(has-edge? 'c 'e) -> #f
+(define (vertices g)
+  (map car g))
+
+; взимане на всички наследници на даден връх в граф
+; (successors 'c G) -> '(a d)
+(define (successors v g)
+  (let ((result (assoc v g)))
+    (if result (cdr result) '())))
+
+; проверка дали съществува ребро м/у два върха в граф
+; (has-edge? 'c 'e G) -> #f
+(define (has-edge? u v g)
+  (member? v (successors u g)))
