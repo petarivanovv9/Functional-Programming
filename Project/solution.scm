5b89c0e9cd24a229478fc0a035e71108c85d4223
Petar Ivanov <pepincho95@gmail.com>
2017-01-15 19:39:35 +0200

    Format Project solution.

--- Project/solution.scm
+++ Project/solution.scm
@@ -1,39 +1,20 @@
-(#%require (only racket/base random))
-(#%require (only racket/base string-titlecase))
-;(#%require (only racket/base read-string))
-;(#%require (only racket/base read-bytes))
-
+#lang racket
 
 (define temp '(("към небето" "д") ("куче" "п" "ср") ("лае" "с" "ед")
                ("птиците" "д") ("зелените" "о" "мн") ("жаби" "п" "мн")
                ("гледат" "с" "мн") ("голямото" "о" "ср")))
 
-(define (my-filter pred? l)
-  (cond
-    ((null? l) '())
-    ((not (pred? (car l))) (my-filter pred? (cdr l)))
-    (else (cons (car l) (my-filter pred? (cdr l))))
-  )
-)
-
-(define (my-map f l)
-  (if (null? l)
-      '()
-      (cons (f (car l)) (my-map f (cdr l)))
-  )
-)
-
 (define (extractNouns l)
-  (my-filter (lambda (x) (if (string=? (cadr x) "п") #t #f)) l))
+  (filter (lambda (x) (if (string=? (cadr x) "п") #t #f)) l))
 
 (define (extractAdjectives l)
-  (my-filter (lambda (x) (if (string=? (cadr x) "о") #t #f)) l))
+  (filter (lambda (x) (if (string=? (cadr x) "о") #t #f)) l))
 
 (define (extractVerbs l)
-  (my-filter (lambda (x) (if (string=? (cadr x) "с") #t #f)) l))
+  (filter (lambda (x) (if (string=? (cadr x) "с") #t #f)) l))
 
 (define (extractAdverbs l)
-  (my-filter (lambda (x) (if (string=? (cadr x) "д") #t #f)) l))
+  (filter (lambda (x) (if (string=? (cadr x) "д") #t #f)) l))
 
 (define on-of (lambda (l) (random-elem l)))
 
@@ -42,11 +23,11 @@
 
 (define nth-elem
   (lambda (n l)
-    (cond ( (= n 0) (car l) )
-          ( else (nth-elem (- n 1) (cdr l))) )))
+    (cond [(= n 0) (car l)]
+          [else (nth-elem (- n 1) (cdr l))])))
 
 
-; reading input
+;reading input
 ;(define temp (call-with-input-file "input.txt" read))
 ;(define temp (file->lines "input.txt"))
 
@@ -65,20 +46,12 @@
   (if (string=? (caddr adj) (caddr noun))
       #t #f))
 
-;idea
-;(define singular (....))
-;(member singular ... and (caddr verb))
-
+; тук може да изнеса тези caddr, caddr в отделни ф-ии
+; като например isPlural, род и т.н
 (define (check-noun-verb noun verb)
-  (if
-   (or (and (or (or (string=? (caddr noun) "м") (string=? (caddr noun) "ж")) (string=? (caddr noun) "ср")) (string=? (caddr verb) "ед"))
-       (and (string=? (caddr noun) "мн") (string=? (caddr verb) "мн"))
-   )
-      #t
-      #f
-  )
-)
-
+  (if (or (and (or (or (string=? (caddr noun) "м") (string=? (caddr noun) "ж")) (string=? (caddr noun) "ср")) (string=? (caddr verb) "ед"))
+          (and (string=? (caddr noun) "мн") (string=? (caddr verb) "мн")))
+      #t #f))
 
 (define getRandomSentence-format1
   (lambda ()
@@ -89,10 +62,7 @@
 
     (if (and (check-adj-noun adjective noun) (check-noun-verb noun verb))
         (apply string-append "" (append (list (string-titlecase (car adjective)) " " (car noun)) (list " " (car verb) " " (car adverb) ".")))
-        (getRandomSentence-format1)
-    )
-  )
-)
+        (getRandomSentence-format1))))
 
 (define getRandomSentence-format2
   (lambda ()
@@ -103,30 +73,21 @@
 
     (if (and
           (and (not (string=? (car (car two-nouns)) (car (cdr two-nouns))))
-               (and (check-adj-noun (car two-adjectives) (car two-nouns)) (check-adj-noun (cdr two-adjectives) (cdr two-nouns)))
-          )
+               (and (check-adj-noun (car two-adjectives) (car two-nouns)) (check-adj-noun (cdr two-adjectives) (cdr two-nouns))))
           (string=? (caddr verb) "мн"))
         (apply string-append "" (append
                                  (list (string-titlecase (car (car two-adjectives))) " " (car (car two-nouns)) " и " (car (cdr two-adjectives)) " " (car (cdr two-nouns)))
                                  (list " " (car verb) " " (car adverb) ".")))
-        (getRandomSentence-format2)
-    )
-  )
-)
+        (getRandomSentence-format2))))
 
 (define getRandomSentence
   (lambda ()
     (with-output-to-file
       "result.txt"
       (lambda ()
-        (write
+        (display
           (if (= (random 2) 0)
             (getRandomSentence-format1)
-            (getRandomSentence-format2)
-          )
-        )
-      )
-      #:exists `replace
-    )
-  )
-)
+            (getRandomSentence-format2)))
+        (newline))
+      #:exists `append)))
