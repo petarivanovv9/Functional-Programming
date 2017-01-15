890b24eae6e69e2390f37c657ff46334eb53dc01
Petar Ivanov <pepincho95@gmail.com>
2017-01-15 19:06:34 +0200

    Add task-2 from Homework-1.

--- Homework-1/solution.hs
+++ Homework-1/solution.hs
@@ -6,10 +6,9 @@
 
 --2) [
 --  ["safe", "strings"], -> наобратно добавяме думите които могат да се конкатенират
-                        -- и винаги ще взимаме head-а
+--                          и винаги ще взимаме head-а
 --  ["strings", "setting"],
 --  ["safe", "elon"],
---  ....
 --]
 
 --3) [
@@ -27,15 +26,10 @@ canConcat s1 s2 = last s1 == head s2
 
 getMaxListWords words = foldr ( \ x res -> if getLengthList x > getLengthList res then x else res ) [] words
 
---връща ни дължината на списък от думи
---foldr приема двуместна ф-ия като параметър
 getLengthList lst = foldr ( \ x res -> length x + res ) 0 lst - length lst + 1
 
---1)'
 makeStrList lst = [ [x] | x<-lst ]
 
---2)'
--- взимаме елемент от lst и се опитваме да го конкатенираме към res
 makeWord lst res = [ x : y | x<-lst, y<-res, x /= head y, canConcat x  (head y) ]
 
 genListStrings lst res currMaxList
@@ -45,14 +39,13 @@ genListStrings lst res currMaxList
   where newRes = makeWord lst res
         currMax = getMaxListWords newRes
 
-
 longestWord lst
   | null wordsList = []
   | otherwise = foldr1 ( \ x (_ : res) -> x ++ res ) wordsList
   where wordsList = (genListStrings lst (makeStrList lst) [])
 
 
--- Task 2
+--Task 2
 
 isPrime 1 = False
 isPrime n = null [ d | d<-[2..(n-1) ], mod n d == 0 ]
@@ -61,6 +54,10 @@ primes = [ x | x<-[2..99], isPrime x ]
 
 isSquare n = not (null [ x | x<-[1..n], x*x == n ])
 
-squares = [ x | x<-[2..99], isSquare x ]
+squares = [ x | x<-[1..99], isSquare x ]
 
---myFunc = length ( [ x | x<-[2..99]  ] )
+oddNums = [ x | x<-[10..99], odd x, not(isPrime x) ]
+
+myFunc = [ x | x<-[10..99], y<-primes, z<-squares, x==y+(2*z) ]
+
+result = length([ x | x<-(oddNums), not(x `elem` myFunc) ])
